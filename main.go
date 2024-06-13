11d3f666fa73077e353f5aaf200c788020f1adfd
Phone Aung <phonexaung@gmail.com>
2024-06-13 19:55:52 +0630

    test MongoDB connection

--- main.go
+++ main.go
@@ -1,13 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// bson is binary json format that mongodb uses
 type Todo struct {
-	ID        int    `json:"id"`
+	ID        int    `json:"_id" bson:"_id"`
 	Completed bool   `json:"completed"`
 	Body      string `json:"body"`
 }
 
+var collection *mongo.Collection
+
 func main() {
 	fmt.Println("Hello World")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file!", err)
+	}
+
+	// get mongodb uri from .env
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+
+	// connect to mongo db connection string
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+
+	// get client and error out of this call
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB Atlas")
 }
