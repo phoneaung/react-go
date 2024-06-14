99e3bd550b47611abd47d00f59d9ae730e2b38cf
Phone Aung <phonexaung@gmail.com>
2024-06-14 21:53:21 +0630

    add delete a todo

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func main() {
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos/", createTodo)
 	app.Patch("/api/todos/:id", updateTodo)
-	// app.Delete("/api/todos:id", deleteTodo)
+	app.Delete("/api/todos:id", deleteTodo)
 
 	// listen to port
 	port := os.Getenv("PORT")
@@ -146,6 +146,24 @@ func updateTodo(c *fiber.Ctx) error {
 	}
 
 	// if all goes well, show a success message
-	return c.Status(200).JSON(fiber.Map{"msg": "Success!"})
+	return c.Status(200).JSON(fiber.Map{"success": true})
+}
 
+func deleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"msg": "Invalid todo ID!"})
+	}
+
+	filter := bson.M{"_id": objectID}
+
+	_, err = collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
