f59d3ed9c8e3b8abad95e347e5844e58ee2c2fba
Phone Aung <phonexaung@gmail.com>
2024-06-23 10:06:57 +0630

    add delete todo

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -29,6 +29,26 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
             queryClient.invalidateQueries({queryKey: ["todos"]});
         }
     });
+
+    const { mutate: deleteTodo, isPending: isDeleting } = useMutation({
+        mutationKey: ["deleteTodo"],
+        mutationFn: async () => {
+            try {
+                const res = await fetch(BASE_URL + `/todos/${todo._id}`, {method: "DELETE",});
+                const data = await res.json();
+                if (!res.ok) {
+                    throw new Error(data.error || "Something went wrong!");
+                }
+                return data;
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["todos"] });
+        },
+    });
+
 	return (
 		<Flex gap={2} alignItems={"center"}>
 			<Flex
@@ -62,8 +82,9 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
 					{!isUpdating && <FaCheckCircle size={20} />}
                     {isUpdating && <Spinner size={"sm"} />}
 				</Box>
-				<Box color={"red.500"} cursor={"pointer"}>
-					<MdDelete size={25} />
+				<Box color={"red.500"} cursor={"pointer"} onClick={() => deleteTodo()}>
+					{!isDeleting && <MdDelete size={25} />}
+                    {isDeleting && <Spinner size={"sm"} />}
 				</Box>
 			</Flex>
 		</Flex>
