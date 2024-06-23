2ba2bd54663e8fd58e61b3a3e9f7580a6595cbe9
Phone Aung <phonexaung@gmail.com>
2024-06-23 10:50:12 +0630

    ready for deployment

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -29,9 +28,12 @@ var collection *mongo.Collection
 func main() {
 	fmt.Println("Hello World")
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file!", err)
+	if os.Getenv("ENV") != "production" {
+		// load the .env file if not in production
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file!", err)
+		}
 	}
 
 	// get mongodb uri from .env
@@ -61,11 +63,12 @@ func main() {
 
 	app := fiber.New()
 
-	// add cors
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173",
-		AllowHeaders: "Origin,Content-Type,Accept",
-	}))
+	// add cors!
+	// In production we dont really want this cors. As client and server are gonna be on same domain.
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins: "http://localhost:5173",
+	// 	AllowHeaders: "Origin,Content-Type,Accept",
+	// }))
 
 	app.Get("/api/todos/", getTodos)
 	app.Post("/api/todos/", createTodo)
@@ -77,6 +80,10 @@ func main() {
 
 	if port == "" {
 		port = "8000"
+	}
+
+	if os.Getenv("ENV") == "production" {
+		app.Static("/", "./client/dist")
 	}
 
 	log.Fatal(app.Listen("0.0.0.0:" + port))
